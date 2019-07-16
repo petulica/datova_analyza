3a1ebda2788ec102cd7b557247cf4ff73649f348
petra c <cernohlavkovapetra@centrum.cz>
2019-07-17 00:29:50 +0200

    oprava drobností

--- nusl_analyza.py
+++ nusl_analyza.py
@@ -1,45 +1,35 @@
 import pandas as pd
 
-df = pd.read_csv('xml2csv/NUSL.csv', sep=';', encoding="Windows-1250") #df = dataframe
+df = pd.read_csv('xml2csv/NUSL.csv', sep=';', encoding="Windows-1250")
 
 print("záznamů celkem", len(df))
 df.duplicated(subset='id', keep='first')
 print("záznamů celkem", len(df))
 
 df_missing_values = (df.isna().sum())
-df_missing_values.to_csv('NUSL/missing_values.csv', sep=';', header=False)
+df_missing_values.to_csv('NUSL_csv/missing_values_NUSL.csv', sep=';', header=False)
 
-#for index, row in df.iterrows():
-#	print(index, row ['projects'])
-#	hodí se, když se potřebuju podívat co je všechno v nějakém sloupci
-
-#celkem 30 653
 
 #úkol_1_a_2
-df['task_1'] = 1
+df['no_of_records'] = 1
 df['year2'] = df['year'].str[:4]
 print(df['year2'])
 
-task_1_df = df.groupby(['year2', 'no_of_projects']).count().astype('Int64')['task_1']
-task_1_df.to_csv('NUSL/projekty_rok.csv', sep=';', header=True)
+task_1_df = df.groupby(['year2', 'no_of_projects']).count().astype('Int64')['no_of_records']
+task_1_df.to_csv('NUSL_csv/projekty_rok_NUSL.csv', sep=';', header=True)
 
 #úkol_2
 
-df['task_2'] = 1
-task_2_df = df.groupby(['langs', 'no_of_projects']).count()['task_2']
-task_2_df.to_csv('NUSL/projekty_jazyk.csv', sep=';', header=True)
+df['no_of_records'] = 1
+task_2_df = df.groupby(['langs', 'no_of_projects']).count()['no_of_records']
+task_2_df.to_csv('NUSL_csv/projekty_jazyk_NUSL.csv', sep=';', header=True)
 #1dok ve více jazycích
 
-df['task_3'] = 1
-task_3_df = df.groupby(['doc_type','no_of_projects']).count()['task_3']
-task_3_df.to_csv('NUSL/projekty_doc_type.csv', sep=';', header=True)
+df['no_of_records'] = 1
+task_3_df = df.groupby(['doc_type','no_of_projects']).count()['no_of_records']
+task_3_df.to_csv('NUSL_csv/projekty_doc_type_NUSL.csv', sep=';', header=True)
 
-
-
-#df.loc[~df['návaznosti'].str.contains('P')] - neobsahuje 'P'; ~ se píše alt a 1 na nenumerické klávesnici
-
-
-"""
-
-neopravovat a nesplitovat v rámci pole 999, ale říct, že tam takový problém je.
-"""
+# df.loc[~df['návaznosti'].str.contains('P')] - neobsahuje 'P'; ~ se píše alt a 1 na nenumerické klávesnici
+# for index, row in df.iterrows():
+# 	print(index, row ['projects'])
+# 	hodí se, když se potřebuju podívat co je všechno v nějakém sloupci
